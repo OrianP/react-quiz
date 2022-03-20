14bf7ec371504f7f68733a7f1ebbd5c7df13d3d6
orian <77785778+OrianP@users.noreply.github.com>
2022-03-20 13:16:39 +0000

    Project setup
    * Create filesystem structure
    * Create helper functions
    * Set state variables
    * Create API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// components
+import QuestionCard from "./components/QuestionCard";
+// functions
+import { fetchQuizQuestions } from "./API";
+// types
+import { Difficulty } from "./API";
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
+	console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+	const startTrivia = async () => {};
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+	const nextQuestion = () => {};
+
+	return (
+		<div className="App">
+			<h1>React Quiz</h1>
+			<button className="start" onClick={startTrivia}>
+				Start
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading questions...</p>
+			{/* <QuestionCard
+				questionNum={number + 1}
+				totalQuestions={TOTAL_QUESTIONS}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+			/> */}
+			<button className="next">Next question</button>
+		</div>
+	);
 }
 
 export default App;
